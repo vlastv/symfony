a9edd35960d865b016761073ab09dcb2cb6e4a48
Vladislav Vlastovskiy <me@vlastv.ru>
2025-11-11 12:30:41 +0300

    Hotfix docs

--- src/Symfony/Component/Webhook/Client/AbstractRequestParser.php
+++ src/Symfony/Component/Webhook/Client/AbstractRequestParser.php
@@ -14,6 +14,7 @@ namespace Symfony\Component\Webhook\Client;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestMatcherInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\RemoteEvent\RemoteEvent;
 use Symfony\Component\Webhook\Exception\RejectWebhookException;
 
